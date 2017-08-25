e1d0d690a41a61e6379dc5b35e5f06918ba02505
Andrew Mudrov <m03g94@gmail.com>
2017-08-25 15:49:22 +0300

    Hide cursor in typing

--- login.js
+++ login.js
@@ -51,23 +51,23 @@
         $('.outer_border').css('background','transparent');
         $('#message_div').children().eq(0).remove();
         $('#message_div').children().eq(0).remove();
-        var typed2 = new Typed("#message_div", {strings: ["<span class=\"message\">ПРОВЕРКА</span><span class=\"line\">&boxh;</span>"], typeSpeed: speedType});
+        var typed2 = new Typed("#message_div", {strings: ["<span class=\"message\">ПРОВЕРКА</span><span class=\"line\">&boxh;</span>"], typeSpeed: speedType, showCursor: false});
 
         $('#console_container').append("<div><span id=\"a_string\" class=\"consoletext nomargin\"></span></div>");
-        var typed2 = new Typed("#a_string", {strings: ["// Подключение юзера " + code], typeSpeed: speedType, onComplete: function(self) {$("#a_string").removeAttr('id')}});
+        var typed2 = new Typed("#a_string", {strings: ["// Подключение юзера " + code], typeSpeed: speedType, onComplete: function(self) {$("#a_string").removeAttr('id')}, showCursor: false});
 
         $('#console_container').append("<div><span id=\"b_string\" class=\"consoletext nomargin\"></span><span id=\"e_string\" class=\"consoletext nomargin\"></span></div>");
-        var typed2 = new Typed("#b_string", {strings: ["// Ждём подключения "], startDelay: 1500, typeSpeed: speedType, onComplete: function(self) {$("#b_string").removeAttr('id')}, preStringTyped: function(self) {$('#console_container').animate({scrollTop: $('#console_container').prop('scrollHeight')}, 500);}});
-        var typed2 = new Typed("#e_string", {strings: ["......"], startDelay: 2000, typeSpeed: 500, onComplete: function(self) {$("#e_string").removeAttr('id'); errorAuth()}});
+        var typed2 = new Typed("#b_string", {strings: ["// Ждём подключения "], startDelay: 1500, typeSpeed: speedType, showCursor: false, onComplete: function(self) {$("#b_string").removeAttr('id')}, preStringTyped: function(self) {$('#console_container').animate({scrollTop: $('#console_container').prop('scrollHeight')}, 500);}});
+        var typed2 = new Typed("#e_string", {strings: ["......"], startDelay: 2000, showCursor: false, typeSpeed: 500, onComplete: function(self) {$("#e_string").removeAttr('id'); errorAuth()}});
     };
 
     function errorAuth() {
         $('#message_div').children().eq(0).remove();
         $('#message_div').children().eq(0).remove();
         $('.outer_border').css('background','rgb(100,0,3)');
-        var typed2 = new Typed("#message_div", {strings: ["<span class=\"message error\">ОШИБКА</span>"], typeSpeed: speedType});
+        var typed2 = new Typed("#message_div", {strings: ["<span class=\"message error\">ОШИБКА</span>"], typeSpeed: speedType, showCursor: false});
         $('#console_container').append("<div><span id=\"c_string\" class=\"consoletext nomargin\"></span></div>");
-        var typed2 = new Typed("#c_string", {strings: ["// Ошибка аутентификации."], startDelay: 500, typeSpeed: speedType, preStringTyped: function(self) {$('#console_container').animate({scrollTop: $('#console_container').prop('scrollHeight')}, 500);}, onComplete: function(self) {$("#c_string").removeAttr('id')}});
+        var typed2 = new Typed("#c_string", {strings: ["// Ошибка аутентификации."], startDelay: 500, typeSpeed: speedType, showCursor: false, preStringTyped: function(self) {$('#console_container').animate({scrollTop: $('#console_container').prop('scrollHeight')}, 500);}, onComplete: function(self) {$("#c_string").removeAttr('id')}});
 
         setTimeout(function(){
             $('#message_div').children().eq(0).remove();
@@ -91,7 +91,7 @@
 
     $('#console_container').css('height', lineHeight * 3);
 
-    var typed = new Typed("#first_string", {strings: ["// MarsOS v1.27"], typeSpeed: speedType});
+    var typed = new Typed("#first_string", {strings: ["// MarsOS v1.27"], typeSpeed: speedType, showCursor: false});
 
     $('#console_container').append("<div><span id=\"second_string\" class=\"consoletext nomargin\"></span></div>");
-    var typed2 = new Typed("#second_string", {strings: ["// Защищенное соединение  : Mars Secure Logon"], startDelay: 1500, typeSpeed: speedType});
+    var typed2 = new Typed("#second_string", {strings: ["// Защищенное соединение  : Mars Secure Logon"], startDelay: 1500, typeSpeed: speedType, showCursor: false});
