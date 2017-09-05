18d23c9eb33bc299ee964f93199d569846d9d45d
Andrew Mudrov <m03g94@gmail.com>
2017-09-05 18:14:31 +0300

    Multiple probes clicked fixed (compltely)

--- common.js
+++ common.js
@@ -90,11 +90,11 @@ if (element.hasAttribute('clicked'))
 function clickflag(element) { /* Click on Probe */
   if (!(element.hasAttribute('clicked')))
   {
-        cancelAnimationFrame(glitches);
     $("#noise_image").css('opacity', '1');
     element.setAttribute('src', 'images/flag_active.png');  
     var prev_img = $("img[clicked='true']");
     prev_img.removeAttr('clicked');
+    prev_img.parent().removeClass('glitch');
     $('.dot_img').addClass('invisible');
     prev_img.attr('src', 'images/flag_inactive.png');
 
@@ -132,14 +132,16 @@ function clickflag(element) { /* Click on Probe */
     var start = performance.now();
 
     function glitches(time){
-      var timePassed = time - start;
+      timePassed = time - start;
       if (timePassed > 1000) {
-        element.setAttribute('src', 'images/flag_inactive.png');
-        $('.dot_img').addClass('invisible');
-        $(element).parent().children().eq(0).removeClass('invisible');
+        if (element.hasAttribute('clicked')) {
+          element.setAttribute('src', 'images/flag_inactive.png');
+          $('.dot_img').addClass('invisible');
+          $(element).parent().children().eq(0).removeClass('invisible');
+          $("#probe_image").attr('src', probe_img);
+          $("#noise_image").css('opacity', '0');
+        }
         $(element).parent().removeClass('glitch');
-        $("#probe_image").attr('src', probe_img);
-        $("#noise_image").css('opacity', '0');
         cancelAnimationFrame(glitches);
       } else {
         requestAnimationFrame(glitches);
